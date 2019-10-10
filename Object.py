1b019858b7cec5f6a9f107e76125f95a7a8bd23a
Jack Chambers <jtcroxley@gmail.com>
2019-10-10 11:35:51 -0400

    Add files via upload

--- Object.py
+++ Object.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 resolution = [0,0]
 objects = []
 
@@ -9,34 +9,66 @@ def setRes(x=500,y=500):
 
 def getObjects():
     return objects
+    
 
 class Object:
-    def __init__(self, screen, velX=5, velY=5, img="ball1.png"):
+    def __init__(self, screen, velX=5, velY=5, img="ball1.png", initPos=(0,0), weight = 1):
+        #Init and Object Creation
+        print("CREATED OBJECT "+img)
         self.screen = screen
         self.velX = velX
         self.velY = velY
         self.img = pygame.image.load(img)
         self.rect = self.img.get_rect()
+        self.rect.x = initPos[0]
+        self.rect.y = initPos[1]
+        self.weight = weight
         objects.append(self)
     
     def move(self):
-        moveOnAxis(self, self.velX, 0)
-        moveOnAxis(self, 0, self.velY)
+        #Splits the movement on to two axis for collision detection
+        self.moveOnAxis(self.velX, 0)
+        self.moveOnAxis(0, self.velY)
+        self.render()
+    
+    def render(self):
+        #Blits The Object To The Screen
         self.screen.blit(self.img, self.rect)
-        
     
     def moveOnAxis(self, dx, dy):
-
         self.rect.x += dx
         self.rect.y += dy
+        
+        #Collision with other objects
         for obj in getObjects():
             if self.rect.colliderect(obj.rect) and self != obj:
                 if dx > 0: #Right Side Collision
-                    self.rect.right = obj.rect.left 
+                    self.rect.right = obj.rect.left
+                    self.velX = -self.velX
+                    obj.velX = -obj.velX
                 if dx < 0: #Left Side Collision
                     self.rect.left = obj.rect.right
+                    self.velX = -self.velX
+                    obj.velX = -obj.velX
                 if dy > 0: #Bottom Side Collision
-                    self.rect.bottom = wall.rect.top
+                    self.rect.bottom = obj.rect.top
+                    self.velY = -self.velY
+                    obj.velY = -obj.velY
                 if dy < 0: #Top Side Collision
-                    self.rect.top = wall.rect.bottom
-            
+                    self.rect.top = obj.rect.bottom
+                    self.velY = -self.velY
+                    obj.velY = -obj.velY
+
+        #Self Wall Collision
+        if self.rect.right > resolution[0]:
+            self.rect.right = resolution[0]
+            self.velX = -self.velX
+        if self.rect.left < 0:
+            self.rect.left = 0
+            self.velX = -self.velX
+        if self.rect.bottom > resolution[1]:
+            self.rect.bottom = resolution[1]
+            self.velY = -self.velY
+        if self.rect.top < 0:
+            self.rect.top = 0
+            self.velY = -self.velY
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ pygame.init()
 width = 1000
 height = 1000
 size = width, height
-fps = 1
+fps = 60
 
 bgColor = (83,174,232)
 
@@ -14,19 +14,24 @@ pygame.display.set_caption("Ball Demo")
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 Object.setRes(width, height)
-
-ball1 = Object.Object(screen,5,5,"ball.png")
-ball2 = Object.Object(screen,2,2,"ball2.png")
+#Object.Object(Surface, xVel, yVel, img, initPos, weight)
+ball1 = Object.Object(screen,6,2,"ball.png",(300,300),1)
+ball2 = Object.Object(screen,1,5,"ball2.png",(50,50),2)
+square1 = Object.Object(screen,6,5,"square1.png",(100,100),3)
+square2 = Object.Object(screen,9,4,"square2.png",(500,500),4)
+penta1 = Object.Object(screen,3,4,"penta1.png",(600,600),5)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit();
     
-    #Visuals
-    screen.fill(bgColor)
-    ball1.move()
-    
-    #Mechanics
+    #Objects
+    screen.fill(bgColor)    
+    objects = Object.getObjects()
+    for obj in objects:
+        obj.move()
+
+    #Pygame
     clock.tick(fps)
     pygame.display.flip()
